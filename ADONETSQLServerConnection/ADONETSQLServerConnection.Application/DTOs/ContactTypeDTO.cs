ec1762cd9a6ab8ef42bb3f5d9a7e14644422e497
Emilio Medina Martinez <emilio.medinamartinez@gmail.com>
2024-09-20 12:58:53 -0300

    add Application project - with DTO and Interfaces of repository - add empty DataAccess project

---
+++ ADONETSQLServerConnection/ADONETSQLServerConnection.Application/DTOs/ContactTypeDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace ADONETSQLServerConnection.Application.DTOs
+{
+    public class ContactTypeDTO
+    {
+        public int IdContactType { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ ADONETSQLServerConnection/ADONETSQLServerConnection.Application/DTOs/PageSubTitleDTO.cs
@@ -0,0 +1,19 @@
+﻿namespace ADONETSQLServerConnection.Application.DTOs
+{
+    public class PageSubTitleDTO
+    {
+        public int IdPageSubTitle { get; set; }
+
+        public string SubTitle { get; set; }
+
+        public string SubtitleDescription { get; set; }
+
+        public string ButtonLinkTex { get; set; }
+
+        public string LinkUrl { get; set; }
+
+        public int IdPageTitle { get; set; }
+
+        public PageTitleDTO PageTitle { get; set; }
+    }
+}
