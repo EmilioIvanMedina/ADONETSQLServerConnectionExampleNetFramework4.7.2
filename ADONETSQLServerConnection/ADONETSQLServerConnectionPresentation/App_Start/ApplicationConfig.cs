28919dcf38b8dc29bdedac914fbc0456de2887fb
Emilio Medina Martinez <emilio.medinamartinez@gmail.com>
2024-09-24 15:57:25 -0300

    add dependency injection strategy + singleton object to get the connection string

---
+++ ADONETSQLServerConnection/ADONETSQLServerConnectionPresentation/App_Start/ApplicationConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Configuration;
+
+namespace ADONETSQLServerConnectionPresentation.App_Start
+{
+    public class ApplicationConfig
+    {
+        public string ConnectionString { get;  }
+        public ApplicationConfig()
+        {
+            ConnectionString = ConfigurationManager.ConnectionStrings["WebSiteDatabase"].ConnectionString;
+        }
+    }
+}
