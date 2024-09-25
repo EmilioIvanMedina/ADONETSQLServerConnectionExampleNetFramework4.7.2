b8c06d8dc9177cc6fd9b1963d0ff2aca32578132
Emilio Medina Martinez <emilio.medinamartinez@gmail.com>
2024-09-25 15:34:22 -0300

    WebApp config: connection string info + implementation of IDataBaseAccess + repositori fetching data form Database

---
+++ ADONETSQLServerConnection/ADONETSQLServerConnection.DataAccess/DataAccess/DataBaseAccess.cs
@@ -0,0 +1,87 @@
+﻿using ADONETSQLServerConnection.Application.Interfaces;
+using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace ADONETSQLServerConnection.DataAccess.DataAccess
+{
+    public class DataBaseAccess : IDataBaseAccess
+    {
+        private readonly SqlConnection _connection;
+        private readonly IApplicationConfig _applicationConfig;
+
+        private bool ConnectionIsOpen => _connection != null && _connection.State == ConnectionState.Open;
+
+        public DataBaseAccess(IApplicationConfig applicationConfig)
+        {
+            _applicationConfig = applicationConfig;
+        }
+
+        public void CloseConnection()
+        {
+            if (ConnectionIsOpen)
+            {
+                _connection.Close();
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        public IEnumerable<T> ExecuteQuery<T>(string query)
+        {
+            return TryAutomapping<T>(ExecuteQuery(query));
+        }
+
+        public DataTable ExecuteQuery(string query)
+        {
+            using (SqlConnection connection = new SqlConnection(_applicationConfig.ConnectionString))
+            {
+                connection.Open();
+                using (SqlCommand cmd = new SqlCommand(query, connection))
+                {
+                    var adapter = new SqlDataAdapter(cmd);
+                    var dataTable = new DataTable();
+                    adapter.Fill(dataTable);
+                    return dataTable;
+                }
+            }
+        }
+
+        public void OpenConnection()
+        {
+            if (_connection == null && _connection.State == ConnectionState.Closed)
+            {
+                _connection.Open();
+            }
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                CloseConnection();
+                _connection.Dispose();
+            }
+        }
+
+        private IEnumerable<T> TryAutomapping<T>(DataTable data)
+        {
+            try
+            {
+                var mapConfig = new MapperConfiguration(cfg => cfg.CreateMap<IEnumerable<T>, DataTable>());
+                var mapper = new Mapper(mapConfig);
+                return mapper.Map<IEnumerable<T>>(data);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("This data cannot be automapped.", ex);
+            }
+        }
+    }
+}
--- ADONETSQLServerConnection/ADONETSQLServerConnection.DataAccess/Repositories/ContactRepository.cs
+++ ADONETSQLServerConnection/ADONETSQLServerConnection.DataAccess/Repositories/ContactRepository.cs
@@ -1,14 +1,40 @@
 ﻿using ADONETSQLServerConnection.Application.DTOs;
 using ADONETSQLServerConnection.Application.Interfaces;
+using ADONETSQLServerConnection.DataAccess.DataAccess;
 using System.Collections.Generic;
+using System.Data;
+using System.Linq;
 
 namespace ADONETSQLServerConnection.DataAccess.Repositories
 {
-    internal class ContactRepository : IContactRepository
+    public class ContactRepository : IContactRepository
     {
+        private readonly IDataBaseAccess _dataAccess;
+
+        public ContactRepository(IDataBaseAccess dataAccess)
+        {
+            _dataAccess = dataAccess;
+        }
+
         public IEnumerable<ContactDTO> GetAll()
         {
-            throw new System.NotImplementedException();
+            var contactsDT = _dataAccess.ExecuteQuery("select * from Contact");
+            
+            var contacts = new List<ContactDTO>();
+
+            foreach (DataRow contact in contactsDT.Rows)
+            {
+                contacts.Add(new ContactDTO
+                {
+                    Contact = contact["contact"].ToString(),
+                    ContactName = contact["Contact_Name"].ToString(),
+                    IdContactType = (int)contact["Id_Contact_Type"],
+                    IdContact = (int)contact["Id_Contact"],
+                });
+
+            }
+
+            return contacts;
         }
 
         public ContactDTO GetContact(int id)
